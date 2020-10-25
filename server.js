6165c30970aa8206d95664b4bc7152ec15dc621f
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-10-25 15:21:20 -0500

    edited

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require("./config/passport")
+var session = require("express-session");
+
 
 var db = require("./models");
 
@@ -13,18 +16,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+
+
+
 // Handlebars
 app.engine(
   "handlebars",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: "main",
+    // layoutsDir: __dirname+'/views/'
   })
 );
 app.set("view engine", "handlebars");
 
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/product-api-routes")(app);
+require("./routes/post-api-routes")(app);
 
 var syncOptions = { force: false };
 
