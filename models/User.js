15f0f055e23b0149ec90c51dfbaae7d68b6a988c
Melanie Cisler <65982774+melaniecisler@users.noreply.github.com>
2020-10-25 15:32:32 -0500

    edited

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 module.exports = function(sequelize, DataTypes) {
-    const User = sequelize.define("User", {
+    const User = sequelize.define("User",  {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -12,14 +12,16 @@ module.exports = function(sequelize, DataTypes) {
         password: {
             type: DataTypes.STRING,
             allowNull: false
-        }
+        },
     });
     // comparing unhashed password to entered by the user to the hashed password stored in the database 
-    User.prototype.validpassword = function(password) {
+    User.prototype.validPassword = function(password) {
+        console.log(bcrypt.compareSync(password, this.password));
         return bcrypt.compareSync(password, this.password);
     };
 
     User.addHook("beforeCreate", function(user) {
         user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     })
+return User;
 };
